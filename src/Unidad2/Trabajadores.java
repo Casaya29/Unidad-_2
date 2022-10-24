68bb711131f1d28a4494cabd4e9910ba44285512
Casaya69 <104517524+Casaya69@users.noreply.github.com>
2022-10-23 23:47:48 -0600

    primer commit

---
+++ src/Unidad2/Trabajadores.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Unidad2;
+
+/**
+ *
+ * @author ivandy
+ */
+
+public class Trabajadores {
+    public int id;
+    public String nombre;
+    public String apellido;
+    public String cargo;
+    public int salario;
+    
+    public Trabajadores(int id, String nombre, String apellido, String cargo, int salario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    
+}
---
+++ src/Unidad2/Unidad2.java
@@ -0,0 +1,18 @@
+package Unidad2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author ivandy
+ */
+public class Unidad2 {
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        p.setVisible(true);
+    }
+}
